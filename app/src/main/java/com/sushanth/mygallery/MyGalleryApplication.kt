5115d5dfe775fdd04b957b5901beeccb8710ba27
Sushanth Reddy Daram <sushanth.reddy@hidubai.com>
2025-04-12 15:55:13 +0530

    Added theme to the app and minor UI improvements

--- app/src/main/java/com/sushanth/mygallery/MyGalleryApplication.kt
+++ app/src/main/java/com/sushanth/mygallery/MyGalleryApplication.kt
@@ -1,8 +1,14 @@
 package com.sushanth.mygallery
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyGalleryApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
--- app/src/main/java/com/sushanth/mygallery/utils/BindingAdapters.kt
+++ app/src/main/java/com/sushanth/mygallery/utils/BindingAdapters.kt
@@ -1,20 +1,25 @@
 package com.sushanth.mygallery.utils
 
-import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
+import android.util.TypedValue
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 @BindingAdapter("imageUri")
 fun loadImage(view: ImageView, uri: Uri?) {
+    val typedValue = TypedValue()
+    view.context.theme.resolveAttribute(com.google.android.material.R.attr.colorSurfaceContainer, typedValue, true)
+    val placeholderColor = ContextCompat.getColor(view.context, typedValue.resourceId)
+
     Glide.with(view.context)
         .load(uri)
         .sizeMultiplier(0.8f)
         .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-        .placeholder(ColorDrawable(Color.DKGRAY))
+        .placeholder(ColorDrawable(placeholderColor))
         .dontAnimate()
         .into(view)
 }
